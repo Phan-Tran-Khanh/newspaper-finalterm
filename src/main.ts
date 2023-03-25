3871593cfde5005e3024ce43557b7c668b24e0b6
Lam Tran <minhlam2102002@gmail.com>
2023-03-25 19:44:52 +0700

    feat(logger): add request looger using morgan

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,31 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import * as hbs from 'hbs';
 import { join } from 'path';
+import * as morgan from 'morgan';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
+  // set up view engine
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
 
+  // set up request logger
+  app.use(
+    morgan('tiny', {
+      stream: {
+        write: (message) => Logger.log(message.replace('\n', '')),
+      },
+    }),
+  );
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
 
+  // set up swagger
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Newspaper API')
     .setDescription('The newspaper API description')
