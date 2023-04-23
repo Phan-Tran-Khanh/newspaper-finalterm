61ee8ebf8020d9768fbafc9fd5bd641e9d0f38f8
Bá Duy <119557954+baduy-wd@users.noreply.github.com>
2023-04-23 15:23:59 +0700

    standardize code

--- views/editor/app.js
+++ views/editor/app.js
@@ -71,32 +71,28 @@ function LoadPosts() {
         let html_postList = $('<div class="post-list" data-page="'+page+'"></div>'); 
         for (let i=0; i<4; i++){
             let item = posts[i];
-            let html_post = $('<div class="e_Post" data-post-id="'+item['id']+'"></div>');
-            html_post.append('<div class="post-info-s">\
-                            <div class="post-pane-s datetime">\
-                                <span>'+Date().toLocaleString()+'</span>\
-                            </div>\
-                            <div class="post-pane-s category">\
-                                <span>'+item['category']+'</span>\
-                            </div>\
-                            <div class="post-pane-s title">\
-                                <span>'+item['title']+'</span>\
-                            </div>\
-                            <div class="post-pane-s abstract">\
-                                <span>'+item['abstract']+'</span>\
-                            </div>\
-                            <div class="post-pane-s tags">\
-                                <span>tags: '+item['tags']+'</span>\
-                            </div>\
-                        </div>');
-            html_post.append('<div class="post-img-s">\
-                                <img src="'+item['image']+'" class="img-fluid">\
+            let html_post = $('<div class="e_Post card mb-3" data-post-id="'+item['id']+'"></div>');
+            let html_row = $('<div class="row g-0"></div>');
+            html_row.append('<div class="col-md-7 d-flex flex-column post-info-s">\
+                                <div class="card-body">\
+                                    <p class="card-text small text-muted">Dong Nai - 23/4/2023</p>\
+                                    <h6 class="card-subtitle text-muted">'+item['category']+'</h6>\
+                                    <h5 class="card-title mb-2">'+item['title']+'</h5>\
+                                    <p class="card-text small flex-grow-1">'+item['abstract']+'</p>\
+                                </div>\
+                                <div class="card-footer">\
+                                    <p class="card-text small text-muted mt-auto">tags: '+item['tags']+'</p>\
+                                </div>\
                             </div>');
-            html_post.append('<div class="post-btn-del">\
-                                <div class="btn btn-danger">\
+            html_row.append('<div class="col-md-4 post-img-s">\
+                                <img src="'+item['image']+'" class="img-fluid" alt="...">\
+                            </div>');
+            html_row.append('<div class="col-md-1 d-grid post-btn-del">\
+                                <div class="btn btn-danger btn-block">\
                                     <i class="bi bi-trash"></i>\
                                 </div>\
                             </div>');
+            html_post.append(html_row);
             html_postList.append(html_post);
         }
         $(".post-list-area").append(html_postList);
@@ -107,21 +103,30 @@ function LoadPosts() {
         $(this).empty();
         var table = $('<table></table>');
         var row = $('<tr></tr>');
-        row.append('<th>N</th>');
+        row.append('<th class="text-center">N</th>');
         for (let page=1; page<=numPages; page++){
-            row.append('<th>e</th>');
+            row.append('<th class="text-center">e</th>');
         }
         row.append('<th>w</th>');
         row.append('<th>s</th>');
         table.append(row);
 
         row = $('<tr></tr>');
-        row.append('<td>&nbsp;</td>');
+        row.append('<td class="page-item">\
+                        <a class="page-link" href="#" aria-label="Previous">\
+                            <span aria-hidden="true">&laquo;</span>\
+                            <span class="visually-hidden">Previous</span>\
+                        </a>\
+                    </td>');
         for (let page=1; page<=numPages; page++){
-            row.append('<td><a href="#">'+page+'</a></td>');
+            row.append('<td class="page-item"><a class="page-link" href="#">'+page+'</a></td>');
         }
-        row.append('<td>&nbsp;</td>');
-        row.append('<td>&nbsp;</td>');
+        row.append('<td colspan="2" class="page-item">\
+                        <a class="page-link" href="#" aria-label="Next">\
+                            <span aria-hidden="true">&raquo;</span>\
+                            <span class="visually-hidden">Next</span>\
+                        </a>\
+                    </td>');
         table.append(row);
 
         $(this).append(table);
