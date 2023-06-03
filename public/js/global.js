1f9300ee1944b74acd0f648d54de7daff803c076
Lam Tran <minhlam2102002@gmail.com>
2023-06-03 15:34:50 +0700

    feat(view): refactor naming

--- public/js/global.js
+++ public/js/global.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    // Switch to the corresponding tab when clicking on a tab link in Login/Register Modals
-    $('#login-register-tabs a').on('click', function (e) {
-        e.preventDefault();
-        $(this).tab('show', 'active');
-    });
+  // Switch to the corresponding tab when clicking on a tab link in Login/Register Modals
+  $('#login-signup-tabs a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show', 'active');
+  });
 
-    // Hide #user-btn and show #anonymity-btn by default
-    $('#user-btn').hide();
+  // Hide #user-btn and show #anonymity-btn by default
+  $('#user-btn').hide();
+  $('#anonymity-btn').show();
+
+  // Event handler for login/register buttons
+  $('#login-btn, #signup-btn').on('click', function () {
+    $('#anonymity-btn').hide();
+    $('#user-btn').show();
+  });
+
+  // Event handler for logout button
+  $('#logout-btn').on('click', function () {
     $('#anonymity-btn').show();
-
-    // Event handler for login/register buttons
-    $('#login-btn, #register-btn').on('click', function () {
-        $('#anonymity-btn').hide();
-        $('#user-btn').show();
-    });
-
-    // Event handler for logout button
-    $('#logout-btn').on('click', function () {
-        $('#anonymity-btn').show();
-        $('#user-btn').hide();
-    });
+    $('#user-btn').hide();
+  });
 });
--- src/utils/hbs.ts
+++ src/utils/hbs.ts
@@ -7,11 +7,15 @@ export default function registerHelpers(hbs: typeof hbsModule) {
       return a == b ? options.fn(this) : options.inverse(this);
     },
   );
-  hbs.registerHelper('times', (n, block) => {
-    let accum = '';
-    for (let i = 1; i <= n; i++) {
-      accum += block.fn(i);
+  hbs.registerHelper('times', (n, options) => {
+    const start = 1;
+    const end = parseInt(n, 10);
+    let result = '';
+
+    for (let i = start; i <= end; i++) {
+      result += options.fn(i);
     }
-    return accum;
+
+    return result;
   });
 }
