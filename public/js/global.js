9d4ba3c7a06b1fbd6263f8570db9c4b82547cc48
Khanh Phan Trần <ptrankhanh02@gmail.com>
2023-06-03 16:35:13 +0700

    minor fix role

--- public/js/global.js
+++ public/js/global.js
@@ -4,20 +4,4 @@ $(document).ready(function () {
     e.preventDefault();
     $(this).tab('show', 'active');
   });
-
-  // Hide #user-btn and show #anonymity-btn by default
-  $('#user-btn').hide();
-  $('#anonymity-btn').show();
-
-  // Event handler for login/register buttons
-  $('#login-btn, #signup-btn').on('click', function () {
-    $('#anonymity-btn').hide();
-    $('#user-btn').show();
-  });
-
-  // Event handler for logout button
-  $('#logout-btn').on('click', function () {
-    $('#anonymity-btn').show();
-    $('#user-btn').hide();
-  });
 });
