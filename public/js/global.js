d24b8e94bdfdd072775ec93adfaeaa460f6ddf48
Khanh Phan Trần <ptrankhanh02@gmail.com>
2023-06-05 22:25:34 +0700

    validate inputted password

--- public/js/global.js
+++ public/js/global.js
@@ -11,24 +11,58 @@ $(document).ready(function () {
   // Logout submitting form
   $('#logout-btn').on('click', function (e) {
     e.preventDefault();
-    $('#logout-form').trigger( "submit" );
+    $('#logout-form').trigger("submit");
   });
 
+  // Reset Password
   $('#forgotpw-form').on('submit', function (e) {
     $('#modalResetPassword').modal('show');
   });
 
-  $('#signup-btn').on('click', function(e) {
-    var newPassword = $('#signup-password').val();
-    var confirmPassword = $('#signup-repassword').val();
-    var errorMessage = $('#pws-match-err');
-  
-    if (newPassword !== confirmPassword) {
-      // Error: Passwords do not match
-      errorMessage.show();
+  // Validate passwords inputted
+  $('#pws-match-err').hide();
+
+  $('#signup-btn').on('click', function (e) {
+    let password = $('#signup-password').val();
+    let confirmPassword = $('#signup-repassword').val();
+    let errorMessage = null;
+
+    // Password length should be at least 8 characters
+    if (password.length < 8) {
+      errorMessage = "Password length should be at least 8 characters."
+    }
+
+    // Password should contain at least one uppercase letter
+    if (!/[A-Z]/.test(password)) {
+      errorMessage = "Password should contain at least one uppercase letter.";
+    }
+
+    // Password should contain at least one lowercase letter
+    if (!/[a-z]/.test(password)) {
+      errorMessage = "Password should contain at least one lowercase letter.";
+    }
+
+    // Password should contain at least one digit
+    if (!/[0-9]/.test(password)) {
+      errorMessage = "Password should contain at least one digit.";
+    }
+
+    // Password should contain at least one special character
+    if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
+      errorMessage = "Password should contain at least one special character.";
+    }
+
+    // Error: Passwords do not match
+    if (password !== confirmPassword) {
+      errorMessage = "Passwords do not match.";
+    }
+
+    if (errorMessage) {
+      $('#pws-err').text(errorMessage);
+      $('#pws-err').show();
       e.preventDefault(); // Prevent form submission
     } else {
-      errorMessage.hide();
+      $('#pws-err').hide();
     }
-  });  
+  });
 });
