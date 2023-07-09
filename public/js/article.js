0438ae51224b1c0d3b96a8da421354dafa88499d
Khanh Phan-Tran <ptrankhanh@gmail.com>
2023-07-10 00:14:40 +0700

    js comment section and fix writing article

--- public/js/article.js
+++ public/js/article.js
@@ -1,45 +1,41 @@
 $(document).ready(function () {
     /* Add New Comment */
     $("#comment-input").on('submit', function (e) {
-        // Prevent comment being submitted
         e.preventDefault();
-        // Create a new comment element
-        let newComment = $("<div>", { "class": "card" }).append(
-            $("<div>", { "class": "card-body" }).append(
-                $("<div>", { "class": "media" }).append(
-                    $("<img>", { "src": "https://via.placeholder.com/50x50", "alt": "..." }),
-                    $("<div>", { "class": "media-body" }).append(
-                        $("<h5>", { "class": "mt-0" }).text("DongNike"),
-                        $("<p>").text($("#comment-text").val())
-                    ),
-                    $("<p>", { "class": "card-text small text-muted" }).text("Posted recently")
-                ),
-                $("<div>", { "class": "d-flex align-items-center mt-3" }).append(
-                    $("<a>", { "href": "#", "class": "like-input me-3 position-relative" }).append(
-                        $("<i>", { "class": "bi bi-hand-thumbs-up" }),
-                        " Like",
-                        $("<span>", { "class": "position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" }).append(
-                            "0",
-                            $("<span>", { "class": "visually-hidden" }).text("Total Likes")
+        $.ajax({
+            url: $(this).attr('action'),
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (response) {
+                // Create a new comment element
+                let newComment = $("<div>", { class: "card" }).append(
+                    $("<div>", { class: "card-body" }).append(
+                        $("<div>", { class: "media" }).append(
+                            $("<img>", {
+                                src: $('#account-avatar').attr('src'),
+                                class: "rounded-circle object-fit-cover border border-dark border-2",
+                                style: "width: 50px; height: 50px;"
+                            }),
+                            $("<div>", { class: "media-body" }).append(
+                                $("<h5>", { class: "mt-0" }).text($("#account-name").text()),
+                                $("<p>").text($("#comment-text").val())
+                            ),
+                            $("<p>", { class: "card-text small text-muted" }).text("Posted recently")
                         )
-                    ),
-                    $("<a>", { "href": "#", "class": "reply-old" }).text("Reply")
-                )
-            ),
-            $("<div>", { "class": "card-footer bg-white border-0" }).append(
-                $("<a>", { "href": "#" }).append(
-                    $("<i>", { "class": "bi bi-arrow-return-right" }),
-                    " 0 Replies"
-                )
-            )
-        );
-        $("#comment-container").append(newComment);
-        $("#comment-text").val("");
+                    )
+                );
+                $("#comment-container").append(newComment);
+                $("#comment-text").val("");
+            },
+            error: function () {
+                console.log('Error sending comment.');
+            },
+        });
     });
     /* Add New Comment */
 
     /* Press Like Link */
-    $('.like-input').on('click', function (e) {
+    /* $('.like-input').on('click', function (e) {
         e.preventDefault();
         let badge = $(this).find(".badge");
         let count = parseInt(badge.text());
@@ -54,32 +50,6 @@ $(document).ready(function () {
         }
         // Update the badge count
         badge.text(count);
-    });
+    }); */
     /* Press Like Link */
-
-    /* Press Reply Link */
-    $('.reply-old').on('click', function (e) {
-        e.preventDefault();
-        $(this).closest('.card-body').append(
-            '<form class="d-flex gap-2 mt-2">' +
-            '<textarea class="form-control" rows="1"></textarea>' +
-            '<button class="btn" type="submit">Send</button>' +
-            '</form>'
-        );
-    });
-    /* Press Reply Link */
-
-    /* Press Nested Replies */
-    $(".replies-container").hide();
-
-    // Listen for click event on the "Replies" link
-    $(".nested-replies").on("click", function (e) {
-        e.preventDefault();
-        // Hide the "Replies" link with fadeOut animation
-        $(this).fadeOut();
-
-        // Show the multiple replies container with slideDown animation
-        $(this).siblings('.replies-container').slideDown();
-    });
-    /* Press Nested Replies */
 });
--- public/js/writer.js
+++ public/js/writer.js
@@ -6,100 +6,21 @@ tinymce.init({
   height : "480"
 });
 
-/* // Preview Article Heading
-var headingConfig = {
-  selector: '.tinymce-heading',
-  menubar: false,
-  inline: true,
-  plugins: [
-    'lists',
-    'powerpaste',
-    'autolink'
-  ],
-  toolbar: 'undo redo | bold italic underline',
-  valid_elements: 'strong,em,span[style],a[href]',
-  valid_styles: {
-    '*': 'font-size,font-family,color,text-decoration,text-align'
-  },
-  powerpaste_word_import: 'clean',
-  powerpaste_html_import: 'clean',
-};
-
-// Preview Article Body
-var bodyConfig = {
-  selector: '.tinymce-body',
-  menubar: false,
-  inline: true,
-  plugins: [
-    'link',
-    'lists',
-    'powerpaste',
-    'autolink',
-    'tinymcespellchecker'
-  ],
-  toolbar: [
-    'undo redo | bold italic underline | fontselect fontsizeselect',
-    'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
-  ],
-  valid_elements: 'p[style],strong,em,span[style],a[href],ul,ol,li',
-  valid_styles: {
-    '*': 'font-size,font-family,color,text-decoration,text-align'
-  },
-  powerpaste_word_import: 'clean',
-  powerpaste_html_import: 'clean',
-};
-
-// Preview Article Image
-let imgConfig = {
-  selector: "#tinymce-img",
-  inline: true,
-  plugins: "image",
-  toolbar: "image",
-  images_file_types: 'jpg,svg,webp',
-  images_upload_url: "/your/upload/url" //Server-side handle this
-}
-
-tinymce.init(headingConfig);
-tinymce.init(bodyConfig);
-tinymce.init(imgConfig); */
-
 function previewImg(event) {
   var fileName = URL.createObjectURL(event.target.files[0]);
   $("#preview").attr("src", fileName);
   $("#preview").addClass("banner");
-  $("#uploadImgButton").text("Change Image")
-}
-
-function uploadImgToImgur() {
-  const input = document.getElementById('uploadImage');
-  const file = input.files[0];
-
-  const formData = new FormData();
-  formData.append('image', file);
-
-  fetch('https://api.imgur.com/3/image', {
-    method: 'POST',
-    headers: {
-      Authorization: 'd296f70487afe57', // Imgur API key
-    },
-    body: formData,
-  })
-    .then(response => response.json())
-    .then(data => {
-      const imageUrl = data.data.link;
-      $('#uploadImgUrl').val(imageUrl);
-    })
-    .catch(error => {
-      console.error('Error uploading image:', error);
-    });
+  $("#uploadImgButton").text("Change Image");
 }
 
 $(document).ready(function () {
+  // Pen name and Date Section
   var today = new Date();
   var formattedDate = today.toDateString();
   $("#created-date").append(" - " + formattedDate);
 
-  $("select#label").mousedown(function (e) {
+  // Label Section
+  $("select#label-init").mousedown(function (e) {
     e.preventDefault();
     var select = this;
     var scroll = select.scrollTop;
@@ -107,4 +28,31 @@ $(document).ready(function () {
     setTimeout(function () { select.scrollTop = scroll; }, 0);
     $(select).focus();
   }).mousemove(function (e) { e.preventDefault() });
+
+  // Banner Image Section
+  $('#post-article-btn').on('click', function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'https://api.imgur.com/3/image',
+      type: 'POST',
+      headers: {
+        Authorization: 'Client-ID d296f70487afe57'
+      },
+      data: {
+        image: $('#uploadImage').prop('files')[0]
+      },
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(response) {
+        var imageUrl = response.data.link;
+        console.log('imgurl = ' + imageUrl);
+        $('#uploadImgUrl').val(imageUrl);
+        $('#article-form').submit();
+      },
+      error: function() {
+        console.log('Error uploading image.');
+      }
+    });
+  });
 });
